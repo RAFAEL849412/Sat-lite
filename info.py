748c15c211e0f6aa8ee01f110055ac2fea1e98a4
RAFAEL849412 <ryanhot.paly@gmail.com>
2025-03-30 20:19:16 -0300

    Update and rename rede.py to info.py

---
+++ info.py
@@ -0,0 +1,20 @@
+# Exemplo de mensagem sobre Starlink em Python
+
+def mensagem_starlink():
+    mensagem = """
+    Starlink: Conectando o Mundo com Internet de Alta Velocidade via Satélites
+
+    Starlink é um projeto da SpaceX, projetado para fornecer acesso à internet de alta velocidade em regiões remotas e áreas de difícil acesso. Utilizando uma rede de satélites de órbita baixa (LEO), o serviço promete reduzir a latência e fornecer conexão de qualidade para milhões de pessoas ao redor do mundo.
+
+    Benefícios do Starlink:
+    - Conexão de alta velocidade.
+    - Baixa latência devido à órbita baixa dos satélites.
+    - Acesso à internet em áreas remotas.
+
+    O futuro do Starlink é promissor, com planos de expansão para oferecer cobertura global e possibilitar a conexão de mais pessoas em regiões onde a internet é limitada.
+
+    """
+    print(mensagem)
+
+# Chamada da função
+mensagem_starlink()
