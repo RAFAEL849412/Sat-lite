51ed24e905987e2160442f62411274013a883cde
RAFAEL849412 <ryanhot.paly@gmail.com>
2025-03-30 16:48:24 -0300

    Update and rename satellite.ini to mensagem.py

---
+++ mensagem.py
@@ -0,0 +1,4 @@
+def exibir_mensagem(): mensagem = ( "Os satélites são dispositivos artificiais colocados em órbita ao redor da Terra " "ou de outros corpos celestes. Eles são usados para diversas finalidades, " "como telecomunicações, navegação, monitoramento do clima e exploração científica. " "Desde o lançamento do Sputnik 1 em 1957, os satélites têm desempenhado um papel " "fundamental no avanço da tecnologia e da ciência." ) print(mensagem)
+
+if name == "main": exibir_mensagem()
+
