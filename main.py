7753ede7ae34b5ecba21e196f7232800a8c5f43f
RAFAEL849412 <ryanhot.paly@gmail.com>
2025-05-19 16:03:30 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -141,7 +141,6 @@ except Exception:
 
 # Variáveis de ambiente e constantes
 roblox_session = requests.Session()
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
